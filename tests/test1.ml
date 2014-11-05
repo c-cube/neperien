3ca3358c12671b0c8b79a01de97bf81f15fb96ac
Simon Cruanes <simon.cruanes.2007@m4x.org>
2014-11-05 02:25:10 +0100

    fix test

--- tests/test1.ml
+++ tests/test1.ml
@@ -6,9 +6,8 @@ let file = "/tmp/yolog";;
 let log = match StructLog.log_to_file file with
   | `Error msg -> failwith msg
   | `Ok x -> x;;
-let a = StructLog.make log "a";;
-StructLog.within log a
-  (fun () ->
+StructLog.within log "a"
+  (fun _id ->
     let b = StructLog.make log "b" in
     let _c = StructLog.make log ~causes:[b] "c" in
     ()
